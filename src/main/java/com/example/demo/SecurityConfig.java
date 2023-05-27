bba24bbc2b5719da522600511320df34b83d71a1
Hiroyuki-Nishi270 <nishi2015_1107@yahoo.co.jp>
2023-05-28 01:11:47 +0900

    3-2. 指定したユーザー名とパスワードでログイン

---
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService user() {
+        UserDetails user = User.builder()
+                .username("yama3")
+                .password(passwordEncoder().encode("123456"))
+                .roles("USER")
+                .build();
+        System.out.println(new BCryptPasswordEncoder().encode("123456"));
+        return new InMemoryUserDetailsManager(user);
+    }
+}
--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -7,8 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class TestController {
+
     @GetMapping
     public String index () {
+
+
         return "index";
     }
 }
