d0c0ed352393391a11acabc321a5b5e06a4ef595
Hiroyuki-Nishi270 <nishi2015_1107@yahoo.co.jp>
2023-05-28 15:28:42 +0900

    7-2. 同一ユーザー名が登録済かのチェックまで完成

--- src/main/java/com/example/demo/SignupForm.java
+++ src/main/java/com/example/demo/SignupForm.java
@@ -1,7 +1,15 @@
 package com.example.demo;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class SignupForm {
+    @NotBlank
+    @Size(min = 1, max = 50)
     private String username;
+
+    @NotBlank
+    @Size(min = 6, max = 20)
     private String password;
 
     public String getUsername() {
