0139f1217ae467bcf905a861035dc2803e8339e1
Hiroyuki-Nishi270 <nishi2015_1107@yahoo.co.jp>
2023-05-28 13:35:54 +0900

    4-2. ログイン画面の作成でログインが完了

--- src/main/java/com/example/demo/SecurityConfig.java
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -32,7 +32,8 @@ public class SecurityConfig {
                                 .loginPage("/login")
                                 .defaultSuccessUrl("/")
                         .permitAll()
-                );
+                )
+                ;
         return http.build();
     }
 
